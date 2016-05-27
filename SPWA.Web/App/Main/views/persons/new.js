61f7a487115e69a051fe5d688bf3bc38d66b8add
wain5188 <3510494@qq.com>
2016-05-27 10:13:38 +0800

    修改添加的html

--- SPWA.Web/App/Main/views/persons/new.js
+++ SPWA.Web/App/Main/views/persons/new.js
@@ -2,13 +2,11 @@
     angular.module('app').controller('spwa.views.person.new', [
         '$scope', '$modalInstance', 'abp.services.app.person',
         function ($scope, $modalInstance, personService) {
-
-            alert('123');
             var vm = this;
             vm.personDto = {};
 
             vm.save = function () {
-                alert('789');
+                
                 personService.addPerson(vm.personDto)
                     .success(function () {
                         abp.notify.info('保存成功!');
@@ -17,7 +15,7 @@
             };
 
             vm.cancel = function () {
-                alert('456');
+               
                 $modalInstance.dismiss();
             };
         }
